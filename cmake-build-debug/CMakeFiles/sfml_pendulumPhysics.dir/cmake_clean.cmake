ca23a998eebbe3ffa080775b5138cd6aad24d805
lutrarutra <artur.gynter@hotmail.com>
2019-09-03 21:22:32 +0300

    Changed IDE from VSCode to CLion

---
+++ cmake-build-debug/CMakeFiles/sfml_pendulumPhysics.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml_pendulumPhysics.dir/src/main.cpp.o"
+  "CMakeFiles/sfml_pendulumPhysics.dir/src/Pendulum.cpp.o"
+  "sfml_pendulumPhysics.pdb"
+  "sfml_pendulumPhysics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml_pendulumPhysics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/sfmltest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfmltest.dir/main.cpp.o"
+  "sfmltest.pdb"
+  "sfmltest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfmltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
